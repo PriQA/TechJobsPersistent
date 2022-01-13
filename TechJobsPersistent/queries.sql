78f0622ca7c3dc473d987e53fc01fa7ade5dd55f
PriQA <priyanka.karpas@gmail.com>
2022-01-13 16:03:55 -0600

    resubmitting

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -10,7 +10,7 @@ SELECT employers.Name, employers.Location
 FROM techjobs.employers
 WHERE employers.Location = "St. Louis City";
 
-OR
+--OR
 
 SELECT employers.Name
 FROM techjobs.employers
@@ -18,4 +18,11 @@ WHERE employers.Location = "St. Louis City";
 
 --Part 3 return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
 --If a skill does not have a job listed, it should not be included in the results of this query.
+
+
+SELECT j.Name as JobName, s.Name SkillName, s.Description
+FROM techjobs.jobskills js 
+INNER JOIN techjobs.jobs j ON js.JobId = j.Id
+INNER JOIN techjobs.skills s on js.SkillId = s.Id
+ORDER BY j.Name asc, s.Name asc;
 
